45a76178783353cb8c9571f67ae62294fadb75c5
pareshsahoo902 <pareshsahoo902@gmail.com>
2020-12-02 17:18:56 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import './App.css';
+var res;
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      first_name:"",
+      last_name:"",
+      email:"",
+      phone_number:"",
+      image_url:"",
+      response:"hhh"
+    }
+  }
+  
+  apicall=(first_name,last_name,email,phone,image)=>{
+    const formData = new FormData()
+    formData.append('firstname', first_name);
+    formData.append('lastname', last_name);
+    formData.append('email', email);
+    formData.append('phonenumber', phone);
+    formData.append('profile_image', image);
+  
+    fetch("https://nodeexcersize.herokuapp.com/api/userModel",{
+      method:"POST",
+      
+      body:formData,
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson)
+       res=responseJson;
+      })
+      .catch(error => {
+      });
+          
+        
+      
+  }
+  render(){
+    console.log(res);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome</h1>
+      <form>
+      <br/>
+      <label class='text-feild'>
+         Select Your Image : 
+      <input type="file" name="name" onChange={(e)=>{this.setState({image_url:e.target.files[0]})}}/>
+      </label>      <br/>
+      <br/>
+      <label class='text-feild'>
+         First Name:
+      <input type="text" name="name" onChange={(e)=>{
+        this.setState({
+          first_name:e.target.value
+        })
+      }}/>
+      </label>
+      <br/>
+      <label class='text-feild'>
+         Last Name:
+      <input  type="text" name="name" onChange={(e)=>{
+        this.setState({
+          last_name:e.target.value
+        })
+      }}/>
+      </label>
+      <br/>
+      <label class='text-feild'>
+         Email ID:
+      <input type="text" name="name" onChange={(e)=>{
+        this.setState({
+          email:e.target.value
+        })
+      }}/>
+      </label>
+      <br/>
+
+      <label class='text-feild'>Phone Number:   
+      <input type="text" name="name" onChange={(e)=>{
+        this.setState({
+          phone_number:e.target.value
+        })
+      }}/>
+      </label>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+    
+      <button onClick={()=>{this.apicall(this.state.first_name,this.state.last_name,this.state.email,this.state.phone_number,this.state.image_url)}}>Click</button>
+  </form>
+      
     </div>
   );
 }
+}
+
 
 export default App;
